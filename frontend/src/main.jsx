f56ce4a7ff2ba682c5caae3f8be97f3d1f78aceb
Mayank Rathore <129645237+RATHORE14@users.noreply.github.com>
2025-01-09 21:38:52 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,8 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-import {StoreContextProvider} from "/StoreContext.jsx";
+import StoreContextProvider from "./Context/storeContext";
+
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
